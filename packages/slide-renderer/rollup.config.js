002e45987b3306e684d68977cb16af8c23a6a95d
xuyuanwei <1148090955@qq.com>
2026-05-28 16:40:40 +0800

    feat(slide-renderer): BaseChartElement + Chart + chartOption (echarts as optional peer)

--- packages/slide-renderer/rollup.config.js
+++ packages/slide-renderer/rollup.config.js
@@ -7,6 +7,7 @@ const external = [
   /^react-dom($|\/)/,
   /^motion($|\/)/,
   /^katex($|\/)/,
+  /^echarts($|\/)/,
   /^shiki($|\/)/,
   /^lucide-react($|\/)/,
   /^tinycolor2($|\/)/,
---
+++ packages/slide-renderer/src/elements/chart/BaseChartElement.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import type { PPTChartElement } from '../../types/slides';
+import { ElementOutline } from '../shared/ElementOutline';
+import { Chart } from './Chart';
+
+export interface BaseChartElementProps {
+  elementInfo: PPTChartElement;
+  target?: string;
+}
+
+export function BaseChartElement({ elementInfo, target }: BaseChartElementProps) {
+  return (
+    <div
+      className={`base-element-chart absolute ${target === 'thumbnail' ? 'pointer-events-none' : ''}`}
+      style={{
+        top: `${elementInfo.top}px`,
+        left: `${elementInfo.left}px`,
+        width: `${elementInfo.width}px`,
+        height: `${elementInfo.height}px`,
+      }}
+    >
+      <div
+        className="rotate-wrapper w-full h-full"
+        style={{ transform: `rotate(${elementInfo.rotate}deg)` }}
+      >
+        <div
+          className="element-content w-full h-full"
+          style={{ backgroundColor: elementInfo.fill }}
+        >
+          <ElementOutline
+            width={elementInfo.width}
+            height={elementInfo.height}
+            outline={elementInfo.outline}
+          />
+          <Chart
+            width={elementInfo.width}
+            height={elementInfo.height}
+            type={elementInfo.chartType}
+            data={elementInfo.data}
+            themeColors={elementInfo.themeColors}
+            textColor={elementInfo.textColor}
+            lineColor={elementInfo.lineColor}
+            options={elementInfo.options}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
